532f28b9582c2450c55b7812d9f0b0703983cc82
Steve Schnepp <steve.schnepp@pwkf.org>
2022-09-11 16:55:56 +0200

    setb_bitaddr

--- opcodes.c
+++ opcodes.c
@@ -1213,26 +1213,7 @@ static uint8_t pop_mem(struct em8051 *aCPU)
 static uint8_t setb_bitaddr(struct em8051 *aCPU)
 {
     uint8_t address = OPERAND1;
-    if (address > 0x7f)
-    {
-        // Note: when this instruction is used to test an output pin, the value used
-        // as the original data will be read from the output data latch, not the input pin
-        // -- MCS(r) 51 Microcontroller Family User's Manual
-        uint8_t bitaddr = address & 7;
-        uint8_t bitmask = (1 << bitaddr);
-        address &= 0xf8;        
-        aCPU->mSFR[address - 0x80] |= bitmask;
-        if (aCPU->sfrwrite[address - 0x80])
-            aCPU->sfrwrite[address - 0x80](aCPU, address);
-    }
-    else
-    {
-        uint8_t bitaddr = address & 7;
-        uint8_t bitmask = (1 << bitaddr);
-        address >>= 3;
-        address += 0x20;
-        aCPU->mLowerData[address] |= bitmask;
-    }
+    write_bit_addr(aCPU, address, true);
     PC += 2;
     return 0;
 }
